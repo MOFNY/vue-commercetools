c0f50494d0d0bd7127e05e60195cfa0059b587ca
chrisschuck <chrisschuck@yahoo.com>
2020-01-29 19:43:14 -0500

    Updates the app to use the Products component and adds an APIKey that will be reused on future pages

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app">
+    <Products :APIKey="APIKey" />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Products from "./components/Products.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Products
+  },
+  data: function() {
+    return {
+      APIKey: "gLQTvFN06-RBrMNGtip4p6LJJpA6IKw3"
+    };
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
